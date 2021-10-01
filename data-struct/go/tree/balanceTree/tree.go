4e407963fb1454b37b9f517029163912b4549ea8
hypon <837098975@qq.com>
2021-10-01 12:03:41 +0800

    b-tree

--- data-struct/go/tree/balanceTree/tree.go
+++ data-struct/go/tree/balanceTree/tree.go
@@ -1,109 +1,229 @@
 package main
 
-import (
-	"log"
-)
+import "fmt"
 
-
-type AVLTreeNode struct {
-	value int
-	high  int
-	left  *AVLTreeNode
-	right *AVLTreeNode
+type AVL struct {
+	value int      //值
+	height int     //深度
+	left *AVL      //左子树
+	right *AVL     //右子树
 }
+// https://blog.csdn.net/qq_36183935/article/details/80315808
 
-func main() {
-	array := []int{5, 3, 1, 8, 9, 10, 11, 2, 4, 7, 6, 12}
-	var root *AVLTreeNode
-	for _, v := range array {
-		root = root.InsertNode(v)
-		log.Println("root=> key:", root.value, "high:", root.high)
+//查找元素
+func (t *AVL) Search(value int) bool {
+	if t == nil {
+		return false
 	}
-	log.Println("-----------------")
-	root.PrintSortTree()
-}
-
-
-
-func NewAVLTreeRoot(root int) *AVLTreeNode {
-	return &AVLTreeNode{root, 0, nil, nil}
-}
-
-func (this *AVLTreeNode) InsertNode(v int) *AVLTreeNode {
-	if this == nil {
-		return &AVLTreeNode{v, 0, nil, nil}
+	compare := value - t.value
+	if compare < 0 {
+		return t.left.Search(value)
+	}else if compare > 0 {
+		return t.right.Search(value)
+	}else {
+		return true
 	}
-	if v < this.value {
-		this.left = this.left.InsertNode(v)
-		this.high = getMax(this.left.getNodeHigh(), this.right.getNodeHigh()) + 1
-		if this.left.getNodeHigh()-this.right.getNodeHigh() == 2 {
-			if v < this.left.value {
-				return this.rightRotation()
-			} else {
-				return this.leftRightRotation()
-			}
+}
+
+
+
+func (t *AVL) leftRotate() *AVL {  //左旋转
+	headNode := t.right
+	t.right = headNode.left
+	headNode.left = t
+	//更新结点高度
+	t.height = max(t.left.getHeight(),t.right.getHeight()) + 1
+	headNode.height = max(headNode.left.getHeight(),headNode.right.getHeight()) + 1
+	return headNode
+}
+
+func (t *AVL) rightRotate() *AVL {  //右旋转
+	headNode := t.left
+	t.left = headNode.right
+	headNode.right = t
+	//更新结点高度
+	t.height = max(t.left.getHeight(),t.right.getHeight()) +1
+	headNode.height = max(headNode.left.getHeight(),headNode.right.getHeight()) + 1
+	return headNode
+}
+
+func (t *AVL) rightThenLeftRotate() *AVL {  //右旋转,之后左旋转
+	//以失衡点右结点先右旋转
+	sonHeadNode := t.right.rightRotate()
+	t.right = sonHeadNode
+	//再以失衡点左旋转
+	return t.leftRotate()
+}
+
+func (t *AVL) LeftThenRightRotate() *AVL {  //左旋转,之后右旋转
+	//以失衡点左结点先左旋转
+	sonHeadNode := t.left.leftRotate()
+	t.left = sonHeadNode
+	//再以失衡点左旋转
+	return t.rightRotate()
+}
+
+func (t *AVL) adjust() *AVL {
+	if t.right.getHeight() - t.left.getHeight() == 2 {
+		if t.right.right.getHeight() > t.right.left.getHeight() {
+			t = t.leftRotate()
+		}else {
+			t = t.rightThenLeftRotate()
 		}
-	} else {
-		this.right = this.right.InsertNode(v)
-		this.high = getMax(this.left.getNodeHigh(), this.right.getNodeHigh()) + 1
-		if this.right.getNodeHigh()-this.left.getNodeHigh() == 2 {
-			if v < this.right.value {
-				return this.rightLeftRotation()
-			} else {
-				return this.leftRotation()
-			}
+	}else if t.left.getHeight() - t.right.getHeight() == 2 {
+		if t.left.left.getHeight() > t.left.right.getHeight() {
+			t = t.rightRotate()
+		} else {
+			t = t.LeftThenRightRotate()
 		}
 	}
-	return this
+	return t
 }
 
-func (this *AVLTreeNode) leftRotation() *AVLTreeNode {
-	node := this.right
-	this.right = node.left
-	node.left = this
-	this.high = getMax(this.left.getNodeHigh(), this.right.getNodeHigh()) + 1
-	node.high = getMax(node.left.getNodeHigh(), node.right.getNodeHigh()) + 1
-	return node
+//添加元素
+func (t *AVL) Insert(value int) *AVL {
+	if t == nil {
+		newNode := AVL{value,1,nil,nil}
+		return &newNode
+	}
+	if value < t.value {
+		t.left = t.left.Insert(value)
+		t = t.adjust()
+	}else if value > t.value{
+		t.right = t.right.Insert(value)
+		t = t.adjust()
+	}else {
+		fmt.Println("the node exit")
+	}
+	t.height = max(t.left.getHeight(),t.right.getHeight()) + 1
+	return t
 }
 
-func (this *AVLTreeNode) rightRotation() *AVLTreeNode {
-	node := this.left
-	this.left = node.right
-	node.right = this
-	this.high = getMax(this.left.getNodeHigh(), this.right.getNodeHigh()) + 1
-	node.high = getMax(node.left.getNodeHigh(), node.right.getNodeHigh()) + 1
-	return node
+
+/*删除元素
+*1、如果被删除结点只有一个子结点，就直接将A的子结点连至A的父结点上，并将A删除
+*2、如果被删除结点有两个子结点，将该结点右子数内的最小结点取代A。
+*3、查看是否平衡,该调整调整
+ */
+func (t *AVL) Delete(value int) *AVL {
+	if t ==nil {
+		return t
+	}
+	compare := value - t.value
+	if compare < 0 {
+		t.left = t.left.Delete(value)
+	}else if compare > 0{
+		t.right = t.right.Delete(value)
+	}else { //找到结点,删除结点（）
+		if t.left != nil && t.right != nil {
+			t.value = t.right.getMin()
+			t.right = t.right.Delete(t.value)
+		} else if t.left !=nil {
+			t = t.left
+		}else {//只有一个右孩子或没孩子
+			t = t.right
+		}
+	}
+	if t != nil {
+		t.height = max(t.left.getHeight(),t.right.getHeight()) + 1
+		t = t.adjust()
+	}
+	return t
 }
 
-func (this *AVLTreeNode) leftRightRotation() *AVLTreeNode {
-	this.left = this.left.leftRotation()
-	return this.rightRotation()
+//按顺序获得树中元素
+func (t *AVL) getAll() []int {
+	values := []int{}
+	return addValues(values,t)
 }
 
-func (this *AVLTreeNode) rightLeftRotation() *AVLTreeNode {
-	this.right = this.right.rightRotation()
-	return this.leftRotation()
+//将一个节点加入切片中
+func addValues(values []int,t *AVL) []int {
+	if t != nil {
+		values = addValues(values,t.left)
+		values = append(values,t.value)
+		fmt.Println(t.value,t.height)
+		values = addValues(values,t.right)
+	}
+	return values
 }
 
-func (this *AVLTreeNode) getNodeHigh() int {
-	if this == nil {
+//查找子树最小值
+func (t *AVL) getMin() int {
+	if t == nil {
 		return -1
 	}
-	return this.high
-}
-
-func (this *AVLTreeNode) PrintSortTree() {
-	if this == nil {
-		return
+	if t.left == nil {
+		return t.value
+	} else {
+		return t.left.getMin()
 	}
-	this.left.PrintSortTree()
-	log.Println(this.value)
-	this.right.PrintSortTree()
 }
 
-func getMax(a, b int) int {
+//查找子树最大值
+func (t *AVL) getMax() int {
+	if t == nil {
+		return -1
+	}
+	if t.right == nil {
+		return t.value
+	} else {
+		return t.right.getMax()
+	}
+}
+
+//查找最小结点
+func (t *AVL) getMinNode() *AVL {
+	if t == nil {
+		return nil
+	}else {
+		for t.left != nil {
+			t = t.left
+		}
+	}
+	return t
+}
+
+//查找最大结点
+func (t *AVL) getMaxNode() *AVL {
+	if t == nil {
+		return nil
+	}else {
+		for t.right != nil {
+			t = t.right
+		}
+	}
+	return t
+}
+
+//得到树高
+func (t *AVL) getHeight() int {
+	if t == nil {
+		return 0
+	}
+	return t.height
+}
+
+func max(a int,b int) int{
 	if a > b {
 		return a
+	}else {
+		return b
 	}
-	return b
 }
+
+
+func main() {
+	bsTree := AVL{100,1,nil,nil}
+	newTree := bsTree.Insert(60)
+	newTree = bsTree.Insert(120)
+	newTree = bsTree.Insert(110)
+	newTree = bsTree.Insert(130)
+	newTree = bsTree.Insert(105)
+	fmt.Println(newTree.getAll())
+
+	newTree.Delete(110)
+	fmt.Println(newTree.getAll())
+
+}
+
